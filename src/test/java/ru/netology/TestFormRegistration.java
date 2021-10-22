d9484a6bad6f72a10dce131c3b52da47a3242706
Svet21 <chuyak.svetlana@gmail.com>
2021-10-22 09:24:50 +0500

    Commit update

--- src/test/java/ru/netology/TestFormRegistration.java
+++ src/test/java/ru/netology/TestFormRegistration.java
@@ -6,14 +6,14 @@ import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selectors;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
 
 import java.time.Duration;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 
-import static com.codeborne.selenide.Condition.exactText;
-import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
@@ -21,30 +21,36 @@ class TestFormRegistration {
 
     @BeforeEach
     void setUp() {
-        Configuration.browser = "chrome";
+            Configuration.browser = "chrome";
+        open("http://localhost:9999");
     }
 
     @Test
-    private void shouldOrderFormRegistration() {
-        open("http://localhost:9999");
+    public void shouldOrderFormRegistration() {
 
-        $("[data-test-id=city]").setValue("Екатеринбург");
+
+        $("[data-test-id=city] .input__control").setValue("Екатеринбург");
 
         LocalDate today = LocalDate.now();
         LocalDate data = today.plusDays(3);
-        DateTimeFormatter formatter = DateTimeFormatter.BASIC_ISO_DATE;
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
         String formatDate = data.format(formatter);
-        $("[data-test-id=date]").setValue(formatDate);
+        // $("[data-test-id=date]").setValue(formatDate);
+        $("[data-test-id=date] .input__control").sendKeys(Keys.CONTROL + "A");
+        $("[data-test-id=date] .input__control").sendKeys(Keys.BACK_SPACE);
+        $("[data-test-id=date] .input__control").setValue(formatDate);
 
-        $("[data-test-id=name]").setValue("Петров Василий");
+        $("[data-test-id=name] .input__control").setValue("Петров Василий");
 
-        $("[data-test-id=phone]").setValue("+79990000000");
+        $("[data-test-id=phone] .input__control").setValue("+79990000000");
 
-        $(".checkbox__control").click();
+        //$(".checkbox__control").click();
+       $("div form fieldset label").click();
 
         $(Selectors.byText("Забронировать")).click();
 
         $(Selectors.withText("Успешно")).shouldBe(visible, Duration.ofSeconds(15));
 
+        $("[data-test-id='notification'] .notification__content").shouldHave(text("Встреча успешно забронирована на " + formatDate));
     }
 }
